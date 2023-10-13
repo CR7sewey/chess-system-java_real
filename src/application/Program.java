e29e1c93b4cb315a599eb9ca5fdcbe0ecf478238
CR7sewey <miguelmotacarvalho@gmail.com>
2023-10-13 18:33:49 +0100

    Project created

---
+++ src/application/Program.java
@@ -0,0 +1,11 @@
+package application;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("This is going to be a chess project");
+	}
+
+}
