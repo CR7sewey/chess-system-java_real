f5fa9df23d59171da87b5dfb5c712af610ef4490
CR7sewey <miguelmotacarvalho@gmail.com>
2023-10-14 18:43:09 +0100

    Little imporvement in printing board

--- src/entities_chess/ChessMatch.java
+++ src/entities_chess/ChessMatch.java
@@ -4,6 +4,7 @@ package entities_chess;
 
 import entities_board.Board;
 import entities_board.Position;
+import entities_chess.pieces.King;
 import entities_chess.pieces.Rook;
 
 public class ChessMatch {
@@ -35,11 +36,21 @@ public class ChessMatch {
 	}
 	
 	private void initialSetup() {
-		
-		placeNewPiece('b',6,new Rook(board, Color.WHITE));
-		placeNewPiece('e',8,new Rook(board, Color.BLACK));
-		placeNewPiece('e',1,new Rook(board, Color.WHITE));
+		placeNewPiece('c', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('c', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 1, new King(board, Color.WHITE));
+
+        placeNewPiece('c', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('c', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 8, new King(board, Color.BLACK));
 	}
+	
 	
 
 }
