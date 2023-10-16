187e2ea7088e2f7e8cb589bbaf1aa9ca3cdcf411
CR7sewey <miguelmotacarvalho@gmail.com>
2023-10-16 14:35:11 +0100

    Handling exception and clearing screen

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,11 @@
 package application;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import entities_board.Board;
 import entities_board.Position;
+import entities_chess.ChessException;
 import entities_chess.ChessMatch;
 import entities_chess.ChessPiece;
 import entities_chess.ChessPosition;
@@ -16,6 +18,8 @@ public class Program {
 		ChessMatch chessMatch = new ChessMatch();
 		
 		while (true) {
+			try {
+			UI.clearScreen();
 			UI.printBoard(chessMatch.getPieces());
 			System.out.println();
 			System.out.println("Source: ");
@@ -26,6 +30,17 @@ public class Program {
 			ChessPosition target = UI.readChessPosition(sc);
 			
 			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+			}
+			catch (ChessException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+				
+			}
+			catch (InputMismatchException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+				
+			}
 		}
 		
 	}
--- src/application/UI.java
+++ src/application/UI.java
@@ -28,20 +28,23 @@ public class UI {
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
-	
+	// https://stackoverflow.com/questions/2979383/java-clear-the-console
+	public static void clearScreen() {
+		System.out.print("\033[H\033[2J");
+		System.out.flush();
+	}
+
 	public static ChessPosition readChessPosition(Scanner sc) {
 		try {
 			String s = sc.nextLine();
 			char c = s.charAt(0);
 			int i = Integer.parseInt(s.substring(1));
-			return new ChessPosition(c,i);
-		}
-		catch (RuntimeException e) {
+			return new ChessPosition(c, i);
+		} catch (RuntimeException e) {
 			throw new InputMismatchException("Error reading!");
 		}
 	}
-	
-	
+
 	private static void printPiece(ChessPiece piece) {
 		if (piece == null) {
 			System.out.print("-");
@@ -66,7 +69,5 @@ public class UI {
 		}
 		System.out.println("  a b c d e f g h");
 	}
-	
-
 
 }
